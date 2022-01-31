dfb3a3a9c27bb9ede532ab9dc63535c105be74d5
Brian Wilson <bmwilson93@gmail.com>
2022-01-31 15:58:18 -0700

    Added custom chat functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="72ee3515-6274-4e34-9988-a0c4966eee9b"
+            userName="eightyleek"
+            userSecret="1234"
+            renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps}/>}
+        />
+    )
 }
 
 export default App;
